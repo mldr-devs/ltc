0e9be846a7809f419baa45a7118f583748dd8097
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2025-01-13 17:13:39 +0100

    Code refactor

--- ltc/sim/traffic.py
+++ ltc/sim/traffic.py
@@ -1,13 +1,10 @@
-
 from dataclasses import dataclass
-
 from enum import Enum
-from typing import Tuple,Callable, NamedTuple
+from typing import Tuple, Callable, NamedTuple
 
-import scipy.signal as signal
 import jax
-
 import jax.numpy as jnp
+import scipy.signal as signal
 from jax import tree
 
 from ltc.sim.constants import TAU
@@ -20,24 +17,28 @@ class ModelState:
 
 
 class TrafficModel(NamedTuple):
-    init: Callable[[jax.Array],ModelState]
-    sample: Callable[[ModelState, jax.Array], Tuple[ModelState,jax.Array]]
+    init: Callable[[jax.Array], ModelState]
+    sample: Callable[[ModelState, jax.Array], Tuple[ModelState, jax.Array]]
 
-def _ss_step(x:jax.Array,u:jax.Array, A:jax.Array, B:jax.Array, C:jax.Array, D:jax.Array)->tuple[jax.Array, jax.Array]:
+
+def _ss_step(x: jax.Array, u: jax.Array, A: jax.Array, B: jax.Array, C: jax.Array, D: jax.Array) -> tuple[jax.Array, jax.Array]:
     x_next = A @ x + B * u
     y = C @ x + D * u
     return y.squeeze(), x_next
 
-class InitialStateConf(Enum):
-    ZERO=0
-    NORMAL=1
 
-def cox_traffic(f3dB: float, order:int=1, loc:float=0., scale:float=1.0, initial_state=InitialStateConf.ZERO)->TrafficModel:
-    """ Model factory.
+class InitialStateConf(Enum):
+    ZERO = 0
+    NORMAL = 1
+
+
+def cox_traffic(f3dB: float, order: int = 1, loc: float = 0., scale: float = 1.0, initial_state=InitialStateConf.ZERO) -> TrafficModel:
+    """
+    Model factory.
 
     This function creates a pair of functions used to initialize and sample form traffic model.
     The model is a Cox process.
-    For each txop the number of frames is drawn from Poisson distribution whose rate parameter is controlled by the filtered white noise.
+    For each TXOP the number of frames is drawn from Poisson distribution whose rate parameter is controlled by the filtered white noise.
 
     For filtration, we use Butterworth filter. The :param f3dB: controls the rage of autocompletion in a traffic pattern (lower f results in longer dependence).
 
@@ -48,38 +49,32 @@ def cox_traffic(f3dB: float, order:int=1, loc:float=0., scale:float=1.0, initial
     :param initial_state: How initial state is constructed
     :return: Model
     """
-    fs = 1/TAU
+
+    fs = 1 / TAU
     nyquist = fs / 2
     normalized_cutoff = f3dB / nyquist
     b, a = signal.butter(order, normalized_cutoff, btype='low', analog=False)
 
-
-    A, B, C, D = tree.map(jnp.asarray,signal.tf2ss(b, a))
+    A, B, C, D = tree.map(jnp.asarray, signal.tf2ss(b, a))
     n_states = A.shape[0]
 
     # for scalar signals
-    B, C, D = tree.map(lambda x: x.flatten(),(B,C,D))
+    B, C, D = tree.map(lambda x: x.flatten(), (B, C, D))
 
-
-    def init(key:jax.Array):
+    def init(key: jax.Array) -> ModelState:
         match initial_state:
             case InitialStateConf.ZERO:
                 return ModelState(x=jnp.zeros((n_states,)))
             case InitialStateConf.NORMAL:
                 return ModelState(x=jax.random.normal(key, (n_states,)))
             case _:
-                raise Exception(f'Unknow initial_state {initial_state}')
+                raise Exception(f'Unknown initial_state {initial_state}')
 
-    def step(state:ModelState, key:jax.Array)->tuple[ModelState,jax.Array]:
+    def step(state: ModelState, key: jax.Array) -> tuple[ModelState, jax.Array]:
         state_key, obs_key = jax.random.split(key)
-        u = loc+ scale*jax.random.normal(state_key)
-        y,x = _ss_step(state.x,u, A,B,C,D )
-        n = jax.random.poisson(obs_key,lam=jnp.exp(y))
-        return ModelState(x=x),n
+        u = loc + scale * jax.random.normal(state_key)
+        y, x = _ss_step(state.x, u, A, B, C, D)
+        n = jax.random.poisson(obs_key, lam=jnp.exp(y))
+        return ModelState(x=x), n
 
     return TrafficModel(init, step)
-
-
-def generate_frames(n, t):
-    # TODO implement
-    return jnp.zeros(n, dtype=int)
--- test/sim/test_traffic.py
+++ test/sim/test_traffic.py
@@ -1,21 +1,20 @@
 import unittest
-import scipy.signal as signal
-import numpy as np
 
 import jax
-
 import jax.numpy as jnp
+import numpy as np
+import scipy.signal as signal
 
 from ltc.sim.constants import TAU
-from ltc.sim.generate_frames import _ss_step, cox_traffic
+from ltc.sim.traffic import _ss_step, cox_traffic
 
 
 class TrafficTestCase(unittest.TestCase):
     def test_ss(self):
         dt = 1e-3
-        fs = 1/dt  # Sampling frequency in Hz
-        cutoff = 1/(10*dt)  # Desired 3dB cutoff frequency in Hz
-        order = 2  # Filter order
+        fs = 1 / dt             # Sampling frequency in Hz
+        cutoff = 1 / (10 * dt)  # Desired 3dB cutoff frequency in Hz
+        order = 2               # Filter order
 
         nyquist = fs / 2
         normalized_cutoff = cutoff / nyquist
@@ -26,24 +25,21 @@ class TrafficTestCase(unittest.TestCase):
         A, B, C, D = jax.tree.map(jnp.squeeze, (A, B, C, D))
 
         u = np.random.randn(6)
-        y,_ = signal.lfilter(b, a, u, zi=np.zeros((order,)))
+        y, _ = signal.lfilter(b, a, u, zi=np.zeros((order,)))
 
         x = np.zeros((A.shape[0],))
 
-        def step(x,u):
+        def step(x, u):
             y, x = _ss_step(x, u, A, B, C, D)
+            return x, y.squeeze()
 
-            return x,y.squeeze()
-
-        _,yjax = jax.lax.scan(step,x, u)
+        _, yjax = jax.lax.scan(step, x, u)
 
         self.assertTrue(np.allclose(y, yjax))
 
     def test_cycle(self):
-        model = cox_traffic(f3dB=1/(10*TAU))
-
+        model = cox_traffic(f3dB=1 / (10 * TAU))
         state = model.init(jax.random.key(4))
-
         state, frames = model.sample(state, key=jax.random.key(44))
 
 
