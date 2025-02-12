34a3e0f6f5aea91c51bffde62677ea1b172d91f8
kaszczech <kszczech@agh.edu.pl>
2025-02-12 14:31:12 +0100

    Add penalty for no transmission

--- ltc/sim/process_output.py
+++ ltc/sim/process_output.py
@@ -5,59 +5,93 @@ from ltc.sim.constants import *
 
 
 def no_transmission(args):
-    _, r, _ = args
-    reward = NO_TX_REWARD
-    return reward, r
+    _, _, _, noTx = args
+    return jax.lax.cond(noTx < MAX_NO_TX_WITHOUT_PENALTY, no_transmission_short, no_transmission_long, args)
 
+def no_transmission_short(args):
+    _, r, _, noTx = args
+    reward = SHORT_NO_TX_REWARD
+    noTx = noTx + 1
+    return reward, r, noTx
+
+def no_transmission_long(args):
+    _, _, _, noTx = args
+    return jax.lax.cond(noTx < MAX_NO_TX_WITH_PENALTY, no_transmission_penalty, max_no_transmission, args)
+
+def max_no_transmission(args):
+    _, r, _, noTx = args
+    reward = MAX_NO_TX_REWARD
+    noTx = 0
+    return reward, r, noTx
+def no_transmission_penalty(args):
+    _, _, _, noTx = args
+    return jax.lax.cond(noTx <= NO_TX_WITHOUT_MAX_PENALTY, no_transmission_scale_penalty, no_transmission_max_penalty, args)
+
+def no_transmission_scale_penalty(args):
+    _, r, _, noTx = args
+    scale_index = (noTx - MAX_NO_TX_WITHOUT_PENALTY) / (NO_TX_WITHOUT_MAX_PENALTY - MAX_NO_TX_WITHOUT_PENALTY)
+    reward = LONG_NO_TX_REWARD * scale_index
+    noTx = noTx + 1
+    return reward, r, noTx
+
+def no_transmission_max_penalty(args):
+    _, r, _, noTx = args
+    reward = LONG_NO_TX_REWARD
+    noTx = noTx + 1
+    return reward, r, noTx
 
 def transmission(args):
-    _, _, channel_state = args
+    _, _, channel_state, _ = args
     return jax.lax.cond(channel_state == 1, transmission_without_collision, transmission_with_collision, args)
 
 
 def transmission_with_collision(args):
-    _, r, _ = args
+    _, r, _, _ = args
     return jax.lax.cond(r < MAX_RETRANSMISSION, retransmission, max_retransmission_collision, args)
 
 
 def max_retransmission_collision(_):
     reward = MAX_RETRANSMISSION_PENALTY
     r = 0
-    return reward, r
+    noTx = 0
+    return reward, r, noTx
 
 
 def retransmission(args):
-    _, r, _ = args
+    _, r, _, _ = args
     reward = COLLISION_PENALTY
     r = r + 1
-    return reward, r
+    noTx = 0
+    return reward, r, noTx
 
 
 def transmission_without_collision(args):
-    buffer_state, _, _ = args
+    buffer_state, _, _, _ = args
     return jax.lax.cond(buffer_state == 1, successful_transmission, empty_buffer_transmission, args)
 
 
 def empty_buffer_transmission(args):
-    _, r, _ = args
+    _, r, _, _ = args
     reward = EMPTY_TX_PENALTY
-    return reward, r
+    noTx = 0
+    return reward, r, noTx
 
 
 def successful_transmission(args):
-    _, r, _ = args
+    _, r, _, _ = args
     reward = (TX_REWARD / (r + 1) ** 2)
     r = 0
-    return reward, r
+    noTx = 0
+    return reward, r, noTx
 
 
 def process_output_i(buffer_state, new_buffer_state, action, channel_state, obs):
-    _, _, r = obs[-1]
-    args = (buffer_state, r, channel_state)
-    reward, r = jax.lax.cond(action == 1, transmission, no_transmission, args)
+    _, _, r, noTx = obs[-1]
+    args = (buffer_state, r, channel_state, noTx)
+    reward, r, noTx = jax.lax.cond(action == 1, transmission, no_transmission, args)
 
     channel_state = jnp.abs(channel_state)
-    obs_t = jnp.array([new_buffer_state, channel_state, r])
+    obs_t = jnp.array([new_buffer_state, channel_state, r, noTx])
     obs = jnp.roll(obs, -1, axis=0)
     obs = obs.at[-1].set(obs_t)
 
@@ -66,3 +100,26 @@ def process_output_i(buffer_state, new_buffer_state, action, channel_state, obs)
 
 def process_output(buffer_states, new_buffer_states, actions, channel_state, obs):
     return jax.vmap(process_output_i, in_axes=(0, 0, 0, None, 0))(buffer_states, new_buffer_states, actions, channel_state, obs)
+
+
+if __name__ == "__main__":
+    buffer_states = jnp.array([1, 0, 1, 0])
+    new_buffer_states = jnp.array([0, 0, 1, 0])
+    actions = jnp.array([0, 0, 0, 1])
+    channel_state = -1
+    obs = jnp.array([[
+        [1, 1, 4, MAX_NO_TX_WITHOUT_PENALTY-1],
+        [1, 1, 4, MAX_NO_TX_WITHOUT_PENALTY],
+        [1, 1, 4, MAX_NO_TX_WITHOUT_PENALTY + 1]
+    ]] * 4)
+
+    expected_obs_0 = jnp.array([
+        [1, 1, 4, 0],
+        [1, 1, 4, 0],
+        [0, 1, 4, 0]
+    ])
+    expected_R_0 = -1.0
+
+    result_obs, result_R = process_output(buffer_states, new_buffer_states, actions, channel_state, obs)
+    print(result_obs)
+    print(result_R)
--- test/sim/test_process_output.py
+++ test/sim/test_process_output.py
@@ -1,6 +1,7 @@
 import unittest
 
 import jax.numpy as jnp
+import jax
 
 from ltc.sim.constants import *
 from ltc.sim.process_output import *
@@ -8,68 +9,68 @@ from ltc.sim.process_output import *
 
 class ProcessOutputTestCase(unittest.TestCase):
     def test_no_transmission(self):
-        args = (1, 5, 0)
-        reward, r = no_transmission(args)
+        args = (1, 5, 0, 1)
+        reward, r, noTx = no_transmission(args)
         self.assertEqual(reward, 0.0)
         self.assertEqual(r, 5)
 
     def test_transmission_without_collision_empty_buffer(self):
-        args = (0, 3, 1)
-        reward, r = transmission_without_collision(args)
+        args = (0, 3, 1, 1)
+        reward, r, noTx = transmission_without_collision(args)
         self.assertEqual(reward, EMPTY_TX_PENALTY)
         self.assertEqual(r, 3)
 
     def test_transmission_without_collision_successful(self):
-        args = (1, 2, 1)
-        reward, r = transmission_without_collision(args)
+        args = (1, 2, 1, 1)
+        reward, r, noTx = transmission_without_collision(args)
         self.assertAlmostEqual(reward, TX_REWARD / (2 + 1) ** 2)
         self.assertEqual(r, 0)
 
     def test_transmission_with_collision_retransmission(self):
-        args = (1, 2, -1)
-        reward, r = transmission_with_collision(args)
+        args = (1, 2, -1, 1)
+        reward, r, noTx = transmission_with_collision(args)
         self.assertEqual(reward, COLLISION_PENALTY)
         self.assertEqual(r, 3)
 
     def test_transmission_with_collision_max_retransmission(self):
-        args = (1, MAX_RETRANSMISSION, -1)
-        reward, r = transmission_with_collision(args)
+        args = (1, MAX_RETRANSMISSION, -1, 1)
+        reward, r, noTx = transmission_with_collision(args)
         self.assertEqual(reward, MAX_RETRANSMISSION_PENALTY)
         self.assertEqual(r, 0)
 
     def test_transmission_collision_path(self):
-        args = (1, 7, -1)
-        reward, r = transmission(args)
+        args = (1, 7, -1, 1)
+        reward, r, noTx = transmission(args)
         self.assertEqual(reward, COLLISION_PENALTY)
         self.assertEqual(r, 8)
 
     def test_transmission_successful_path(self):
-        args = (1, 1, 1)
-        reward, r = transmission(args)
+        args = (1, 1, 1, 1)
+        reward, r, noTx = transmission(args)
         self.assertAlmostEqual(reward, TX_REWARD / (1 + 1) ** 2)
         self.assertEqual(r, 0)
 
     def test_transmission_empty_buffer(self):
-        args = (0, 1, 1)
-        reward, r = transmission(args)
+        args = (0, 1, 1, 1)
+        reward, r, noTx = transmission(args)
         self.assertEqual(reward, EMPTY_TX_PENALTY)
         self.assertEqual(r, 1)
 
     def test_process_rl_output(self):
         buffer_states = jnp.array([0, 0, 1, 0])
         new_buffer_states = jnp.array([0, 0, 1, 0])
-        actions = jnp.array([1, 0, 0, 1])
+        actions = jnp.array([1, 0, 1, 1])
         channel_state = -1
         obs = jnp.array([[
-            [1, 1, 6],
-            [1, 1, 7],
-            [1, 1, 8]
+            [1, 1, 6, 0],
+            [1, 1, 7, 0],
+            [1, 1, 8, 0]
         ]] * 4)
 
         expected_obs_0 = jnp.array([
-            [1, 1, 7],
-            [1, 1, 8],
-            [0, 1, 0]
+            [1, 1, 7, 0],
+            [1, 1, 8, 0],
+            [0, 1, 0, 0]
         ])
         expected_R_0 = -1.0
 
@@ -77,7 +78,6 @@ class ProcessOutputTestCase(unittest.TestCase):
 
         self.assertTrue(jnp.array_equal(result_obs[0], expected_obs_0))
         self.assertEqual(result_R[0], expected_R_0)
-
 
 if __name__ == '__main__':
     unittest.main()
