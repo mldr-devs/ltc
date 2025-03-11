04e6abfd80080f9fe66ac45f9fe5fccdaf4b566a
Krzysztof Rusek <krusek@agh.edu.pl>
2025-03-11 11:36:38 +0100

    Add unit tests for StochasticVariationalQNetwork functionality

---
+++ test/test_bayes.py
@@ -0,0 +1,21 @@
+import unittest
+import jax
+import jax.numpy as jnp
+from ltc.agents.drl import QNetwork, StochasticVariationalQNetwork
+
+class BayesTestCase(unittest.TestCase):
+    def test_svi(self):
+        env_state = jnp.zeros((5,4))
+        qn = QNetwork()
+        svi = StochasticVariationalQNetwork(model=qn)
+
+        vars = svi.init(jax.random.key(0), env_state)
+
+        y, s = svi.apply(vars, env_state, rngs=jax.random.key(0), mutable=['loss'])
+        self.assertTrue('loss' in s)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
