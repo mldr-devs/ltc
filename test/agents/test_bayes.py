6b4090089037faa66652e5790bd284ef6f53614d
Krzysztof Rusek <krusek@agh.edu.pl>
2025-07-28 00:08:48 +0200

    Draft multiple samples SVI

--- test/agents/test_bayes.py
+++ test/agents/test_bayes.py
@@ -3,6 +3,24 @@ import jax
 import jax.numpy as jnp
 from ltc.agents.drl import QNetwork, StochasticVariationalNetwork
 
+import flax.linen as nn
+
+class MultiSVI(nn.Module):
+    model: nn.Module
+    num_ensembles: int = 16
+
+    @nn.compact
+    def __call__(self, x):
+        Batched = nn.vmap(
+            StochasticVariationalNetwork,
+            in_axes=(None,), out_axes=-1,
+            variable_axes={'params': None, 'loss':0},
+            split_rngs={'params': False, 'dropout': True, 'posterior': True},
+            axis_size=self.num_ensembles
+        )
+        ensemble = Batched(model=self.model, name='ensemble')
+
+        return ensemble(x)
 
 class BayesTestCase(unittest.TestCase):
     def test_svi(self):
@@ -15,6 +33,14 @@ class BayesTestCase(unittest.TestCase):
 
         self.assertTrue('loss' in s)
 
+    def test_multi_svi(self):
+        env_state = jnp.zeros((5, 4))
 
+        qn = QNetwork()
+        svi = MultiSVI(model=qn)
+        vars = svi.init(jax.random.key(0), env_state)
+        y, s = svi.apply(vars, env_state, rngs=jax.random.key(0), mutable=['loss'])
+
+        self.assertTrue('loss' in s)
 if __name__ == '__main__':
     unittest.main()
