04f8f2f3df6d9d587f421c6f8ac0b2ea130fd433
Krzysztof Rusek <krusek@agh.edu.pl>
2025-11-26 15:17:51 +0100

    Add initial implementation of LTC julia module and setup files

---
+++ LTC.jl/scripts/run.jl
@@ -0,0 +1,21 @@
+
+ENV["JULIA_CONDAPKG_BACKEND"] = "Null"
+ENV["JULIA_PYTHONCALL_EXE"] = joinpath(@__DIR__, "..","..",".venv","bin","python")  # optional
+
+import Pkg
+
+Pkg.activate(joinpath(@__DIR__, ".."))  # activate LTC project
+Pkg.instantiate()
+# Pkg.build("PythonCall")
+
+import LTC
+using PythonCall
+
+
+ltcsym = pyimport("ltc.symbolic")
+print("LTC symbolic imported:", ltcsym)
+
+jax = pyimport("jax")
+jnp = pyimport("jax.numpy")
+jeden=jnp.ones((3,3))
+print("JAX jnp.ones:",jeden)
---
+++ LTC.jl/src/LTC.jl
@@ -0,0 +1,34 @@
+module LTC
+
+
+function qloss(x, y, action)
+    # xx = x[CartesianIndex.(1:length(action), action + 1)]
+    # yy = y[CartesianIndex.(1:length(action), action + 1)]
+    print(x)
+    print(y)
+
+    # return sum
+    index = Int32.(action .+ 1)
+    return @. sum((x[index] - y[index])^2)
+end
+
+function my_loss(tree, dataset::Dataset{T,L}, options)::L where {T,L}
+    prediction, flag = eval_tree_array(tree, dataset.X, options)
+    if !flag
+        return L(Inf)
+    end
+    print(options)
+    return sum((prediction .- dataset.y) .^ 2) / dataset.n
+end
+
+function f()
+
+
+
+
+    return model
+end
+
+
+
+end # module LTC
