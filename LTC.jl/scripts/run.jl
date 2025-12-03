fc75c8cf5c621398077046052c7bb83cb1f7f834
Krzysztof Rusek <krusek@agh.edu.pl>
2025-12-03 11:47:40 +0100

    Basic run in julia

--- LTC.jl/scripts/run.jl
+++ LTC.jl/scripts/run.jl
@@ -1,22 +1,42 @@
-
 ENV["JULIA_CONDAPKG_BACKEND"] = "Null"
 ENV["JULIA_PYTHONCALL_EXE"] = joinpath(@__DIR__, "..","..",".venv","bin","python")  # optional
 
 import Pkg
-
 Pkg.activate(joinpath(@__DIR__, ".."))  # activate LTC project
-# Pkg.instantiate()
-# Pkg.build("PythonCall")
+
 
 import LTC
 using PythonCall
 
 LTC.f()
 
-ltcsym = pyimport("ltc.symbolic")
-print("LTC symbolic imported:", ltcsym)
+function main()
+    P = pyimport("ltc.symbolic.julia")
+    jax = pyimport("jax")
+    jnp = pyimport("jax.numpy")
+    print(P)
+    args = P.steup_args()
+    
+    n = args.n
+    n_drl = args.n_drl
+    n_epochs = args.n_epochs
+    n_steps = args.n_steps
+    window_size = args.window_size
+    seed = args.seed
+    traffic_type = args.traffic_type
 
-jax = pyimport("jax")
-jnp = pyimport("jax.numpy")
-jeden=jnp.ones((3,3))
-print("JAX jnp.ones:",jeden)
+    key = jax.random.key(seed)
+    num_actions =  P._len(P.Actions)
+    actions = jnp.zeros(n, dtype=jnp.int32)
+    buffer_states = jnp.zeros(n, dtype=jnp.int32)
+    power_states = jnp.full(n, P.INITIAL_CAPACITY, dtype=jnp.int32)
+    channel_state = 0
+    obs = P.make_obs(window_size, n)
+    rewards = jnp.zeros(n)
+    terminals = jnp.full(n, false, dtype=jnp.bool)
+
+
+end
+
+
+main()
--- LTC.jl/src/LTC.jl
+++ LTC.jl/src/LTC.jl
@@ -30,4 +30,5 @@ function f()
 end
 
 
+
 end # module LTC
