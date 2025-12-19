917f501f728d75c3321ba46e3b9f113ccf5c0d92
Krzysztof Rusek <krusek@agh.edu.pl>
2025-12-19 12:04:27 +0100

    Coorect log caption

--- LTC.jl/scripts/run.jl
+++ LTC.jl/scripts/run.jl
@@ -68,7 +68,7 @@ function main()
             end
 
             rewards[:, step] = [t.r for t in transitions]
-            @debug "Average reward" avg_reward = sum(transitions .|> t -> t.r)
+            @debug "Total reward" avg_reward = sum(transitions .|> t -> t.r)
 
         end
         reset!(env)
